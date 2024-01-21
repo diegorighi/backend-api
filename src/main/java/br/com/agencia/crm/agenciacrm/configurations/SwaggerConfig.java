f34f8f84ffe82f3b86a288cea6ef874351066a12
Diego Righi <dev.drighi@icloud.com>
2024-01-21 05:49:16 -0300

    Swagger

---
+++ src/main/java/br/com/agencia/crm/agenciacrm/configurations/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package br.com.agencia.crm.agenciacrm.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    
+}
